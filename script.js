f02076bb6e19c70c63c865fe44bb274948362393
JAVID ASHWAQ F <112797329+Javid8048@users.noreply.github.com>
2023-10-01 18:51:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const againBtn = document.querySelector(".againBtn");
+const submitBtn = document.querySelector(".submitBtn");
+const questionMark = document.querySelector(".questionMark");
+const score = document.querySelector(".score");
+const highScore = document.querySelector(".highScore");
+let guessNumber = Math.trunc(Math.random()*20)+1;
+const messageDisplay = document.querySelector(".messageDisplay");
+
+let successBeat = new Audio("./assets/success.mp3");
+let loseBeat = new Audio("./assets/you-lose.mp3");
+
+let highScoreVal = 0;
+let scoreVal = 20;
+
+function restartGame() {
+    successBeat.pause();
+    loseBeat.pause();
+    questionMark.textContent = "?";
+    messageDisplay.textContent = "Start Guessing ...";
+    document.documentElement.style.setProperty('--background', 'black');
+    guessNumber = Math.trunc(Math.random()*20)+1;
+    score.textContent = 20;
+    scoreVal = 20;
+};
+
+againBtn.addEventListener('click', restartGame);
+
+submitBtn.addEventListener('click', () => {
+    let inputBox = Number(document.querySelector(".inputBox").value);
+    console.log(guessNumber);
+    if(inputBox === guessNumber) {
+        questionMark.textContent = guessNumber;
+        questionMark.style.width = "15rem";
+        messageDisplay.textContent = "🎉 Correct Guess";
+        document.documentElement.style.setProperty('--background', 'green');
+        highScoreVal = scoreVal;
+        highScore.textContent = highScoreVal < guessNumber ? scoreVal : highScoreVal;
+        successBeat.play();
+    }
+    else if(inputBox !== guessNumber) {
+        messageDisplay.textContent = inputBox < guessNumber ? "👇 Number is too less" : "👆 Number is too high";
+        scoreVal--;
+        loseBeat.play();
+        if(scoreVal < 1) {
+            restartGame()
+            alert("Game Over"); 
+        };
+    }
+    score.textContent = scoreVal;
+});
+
